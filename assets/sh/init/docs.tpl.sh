ec94e80216443277f0c3d3a7c79c552a4739f8e4
jdlabails <jean-david.labails-exterieur@fiducial.net>
2014-10-16 14:10:05 +0200

    revue generation de la doc

--- assets/sh/init/docs.tpl.sh
+++ assets/sh/init/docs.tpl.sh
@@ -2,7 +2,7 @@
 if [ ${DOC} -eq 1 ]
 then
     echo "Rédaction de la documentation"
-    rm -f ${DIR_REPORT}/DOCS/*.txt
+    rm -rf ${DIR_REPORT}/DOCS/*
     echo "php ${DIR_PHAR}/phpDocumentor.phar -d ${DIR_SRC} -t ${DIR_REPORT}/DOCS" > ${DIR_REPORT}/DOCS/cmd.txt
     php ${DIR_PHAR}/phpDocumentor.phar -d ${DIR_SRC} -t ${DIR_REPORT}/DOCS > ${DIR_REPORT}/DOCS/report.txt 2>&1
 fi
